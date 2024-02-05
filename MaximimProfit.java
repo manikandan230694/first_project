022e0c03b48dedfe8a1c385188204173080dbb77
manikandan230694 <45188723+manikandan230694@users.noreply.github.com>
2024-02-05 16:44:28 +0530

    Add files via upload
    Maximize Profit by trading stocks based on given rate per day

---
+++ MaximimProfit.java
@@ -0,0 +1,32 @@
+package com.OnlineShop.ecom;
+
+public class MaximimProfit {
+
+	public static void main(String[] args) {
+	
+	int[] arr1=new int[] {8,3,1};
+	int profit =maximumProfit(arr1);
+	System.out.println(profit);
+
+	}
+
+	private static int maximumProfit(int[] arr1) {
+		
+		int currentDay=arr1.length-1;
+		int maxProfit=0;
+		
+		
+		while(currentDay>0) {
+			int prevDay=currentDay-1;
+			
+			while(prevDay>=0&&(arr1[currentDay]>arr1[prevDay])) {
+				maxProfit+= arr1[currentDay]-arr1[prevDay];
+				prevDay--;
+			}
+			currentDay=prevDay;
+		}
+		
+		return maxProfit;
+	}
+
+}
